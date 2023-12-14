76a08afb3144dcdf59df493938df03dadac3bf6c
LUKASZ LEMPICKI <lukasz.lempicki@gmail.com>
2023-12-15 00:19:07 +0100

    add: Open Sans font directly from the assets

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,7 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  font-family: "Open Sans", sans-serif;
-}
-
 #app {
-  // font-family: Avenir, Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
-// TODO: try to implement it correctly or remove it later
-// const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // TODO: try to implement it correctly or remove it later
-  // configureWebpack: {
-  //   plugins: [
-  //     new GoogleFontsPlugin({
-  //       fonts: [{ family: "Open Sans" }],
-  //     }),
-  //   ],
-  // },
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = "InBank project";
